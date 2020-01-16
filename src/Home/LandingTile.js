08f42f08acc04d3e3e40d499322ead0063d7054f
Arjun <arjungupta2110@gmail.com>
2020-01-16 18:22:35 +0000

    test post commit hook

--- src/Home/LandingTile.js
+++ src/Home/LandingTile.js
@@ -131,7 +131,7 @@ export default function LandingTile({ infoSectionRef, history }) {
               marginBottom: "1.5em"
             }}
           >
-            React Developer 4
+            React Developer
           </div>
           <Quote>
             "Finding solutions to business problems is more interesting and
