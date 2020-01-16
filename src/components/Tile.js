03c05f0c28262d2b7b65bf86bdb79051fb7d1565
Arjun <arjungupta2110@gmail.com>
2020-01-16 18:15:48 +0000

    Updates

--- src/components/Tile.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Tile = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: ${({ colour }) => colour || "white"};
-`;
-
-export default Tile;
