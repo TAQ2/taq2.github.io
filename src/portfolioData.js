bf047af4a3db9d4052b8945c0bb685a000a99edb
Arjun <arjungupta2110@gmail.com>
2020-02-16 20:43:41 +0000

    update portfolio section to add hindi flashcards project

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -1,15 +1,23 @@
 import fakeLatinNewsImg from "./assets/portfolio/fake-latin-news-screenshot.png";
-import mappingToolImg from "./assets/portfolio/mapping-tool-screenshot.png";
+import hindiFlashcardsImg from "./assets/portfolio/hindi-flashcards.png";
 import websiteImg from "./assets/portfolio/website.png";
 
 const data = [
+  // {
+  //   img: ,
+  //   name: "Mapping Tool",
+  //   description:
+  //     "I wanted to make an app that provokes the concepts of data visualisation. Using the Mapbox library, users can view different metrics wrangled from different sources and navigate using a sleek interface.",
+  //   githubLink: "https://github.com/taq2/mapping-tool",
+  //   appLink: "https://taq2.github.io/mapping-tool"
+  // },
   {
-    img: mappingToolImg,
-    name: "Mapping Tool",
+    img: hindiFlashcardsImg,
+    name: "Hindi Flashcards",
     description:
-      "I wanted to make an app that provokes the concepts of data visualisation. Using the Mapbox library, users can view different metrics wrangled from different sources and navigate using a sleek interface.",
-    githubLink: "https://github.com/taq2/mapping-tool",
-    appLink: "https://taq2.github.io/mapping-tool"
+      "Taking Hindi lessons means that I frequently need to refresh my memory so I decided to use my coding skills to improve my learning experience. Its simple design makes it easy to consume, therefore I have shared it with my fellow peers and am seeing daily use on it.",
+    githubLink: "https://github.com/taq2/hindi-flashcards",
+    appLink: "https://taq2.github.io/hindi-flashcards"
   },
   {
     img: fakeLatinNewsImg,
